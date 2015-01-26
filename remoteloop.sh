14bbbc223a7de7e7b628e3121ef4e18bd328f6d7
Zeeshan Ali Shah <zashah@pdc.kth.se>
2015-01-26 18:47:44 +0100

    Update remoteloop.sh

--- remoteloop.sh
+++ remoteloop.sh
@@ -1,2 +1,6 @@
 # useful for executing remote commands with loop 
 for HOST in cloud{10..19}; do ssh -o StrictHostKeyChecking=no $HOST "sudo service nova-compute restart"  ; done
+
+
+## copy file on remote hosts
+for HOST in ubuntu01 fedora02 centos03 rhel06 ;do scp somefile $HOST:/var/tmp/ ; done
